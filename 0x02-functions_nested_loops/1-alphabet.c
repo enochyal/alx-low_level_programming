bfa0171c163531ecd784d939c4a4a97e9d3eb4ca
Henock Yalew <enochyal@gmail.com>
2022-06-24 08:48:12 -0700

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,20 @@
  * main -> print alphabet a-z
  * Return: On success 0.
  */
+void print_alphabet(void);
 int main(void)
+{
+	char ch;
+
+	print_alphabet();
+	_putchar('\n');
+
+	return (0);
+}
+void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
-
-	return (0);
 }
