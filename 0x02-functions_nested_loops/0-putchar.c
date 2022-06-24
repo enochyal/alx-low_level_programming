20c422579993fb82775cb3da4b160b9a2b218647
Henock Yalew <enochyal@gmail.com>
2022-06-24 07:52:16 -0700

    function and nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-/** main -> print _putchar
+/** main - print _putchar
+ * Return: On success 0.
  *
- *  * Return: On success 0.
- *
- *   */
+ */
 int main(void)
 {
 	_putchar('_');
