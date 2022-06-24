959e2fb1e344dd641eb767f96b9cc49dd788fefa
Henock Yalew <enochyal@gmail.com>
2022-06-24 08:22:13 -0700

    function and nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -> print alphabet a-z
+ * Return: On success 0.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
