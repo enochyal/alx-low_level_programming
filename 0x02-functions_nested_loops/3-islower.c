a7384f8b2006218e19c0abe65fcd181d08ba3e5f
Henock Yalew <enochyal@gmail.com>
2022-06-25 03:25:45 -0700

    nested loop and function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check if  alphabet c if it is in lower case.
+ * @c: parameter used for alphabet lower case check.
+ * Return: success 0.
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
