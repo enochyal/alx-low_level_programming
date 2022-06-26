65c912cab6b8b0a162531261dcee0e2e67c086d9
Henock Yalew <enochyal@gmail.com>
2022-06-26 11:41:58 -0700

    nested loop and function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,23 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+,');
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	if (n == 0)
 	{
-		_putchar('0,');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		return (0);
 	}
-if (n < 0)
-{
-	_putchar('-,');
-	return (-1);
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
 }
