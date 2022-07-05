8dcd3bb4c06ccaf4d926c4a3cfc3fc4652ddcafe
Henock Yalew <enochyal@gmail.com>
2022-07-05 05:57:19 -0700

    more on nested loop and function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57 ; i++)
+	{
+		if (i != 50 && (i != 52)
+				{
+				_putchar(i);
+				_putchar('\n');
+				}
+}
+}
