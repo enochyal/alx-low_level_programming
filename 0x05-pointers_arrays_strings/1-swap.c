c9a0574c488c8a5a96182f7af1988d01e645545d
Henock Yalew <enochyal@gmail.com>
2022-07-17 06:23:28 -0700

    strlen

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,3 +1,10 @@
+#include "main.h"
+
+/**
+ * swap_int - interchange the values of two variables.
+ * @a, @b: parameters.
+ */
+
 void swap_int(int *a, int *b)
 {
 	int tmp;
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** 
+ * _strlen - count the number of strings presents.
+ * @s: parameter.
+ * Return: len.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
