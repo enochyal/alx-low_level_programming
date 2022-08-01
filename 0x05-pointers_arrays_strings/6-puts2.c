46476afe176f92097bf4fb3adcd45aafc1bf1fb1
Henock Yalew <enochyal@gmail.com>
2022-08-01 13:10:06 -0700

    my fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,18 @@
 #include "main.h"
-
 /**
- *
- *
- *
+ * puts2 - > prints every string. * @str: parameter.
+ * @str: parameter.
  */
-void puts2(char *str)
+void puts(char *str)
 {
-	int i;
+	int i, l;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	l = 0;
+	while(s[l] != '\0')
+		l++;
+	for(i=0; i < l/2; i += 2)
+	{
 		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
